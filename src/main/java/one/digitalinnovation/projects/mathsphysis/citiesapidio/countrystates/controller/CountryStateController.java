c6003cbc22435225cbda132c3ecf14655e27725a
Maths Physis <smathsphysis@gmail.com>
2021-08-03 19:02:51 -0400

    feat: Country state service added and listAll method implemented

--- src/main/java/one/digitalinnovation/projects/mathsphysis/citiesapidio/countrystates/controller/CountryStateController.java
+++ src/main/java/one/digitalinnovation/projects/mathsphysis/citiesapidio/countrystates/controller/CountryStateController.java
@@ -1,4 +1,26 @@
 package one.digitalinnovation.projects.mathsphysis.citiesapidio.countrystates.controller;
 
+
+import lombok.AllArgsConstructor;
+import one.digitalinnovation.projects.mathsphysis.citiesapidio.countrystates.entity.CountryState;
+import one.digitalinnovation.projects.mathsphysis.citiesapidio.countrystates.service.CountryStateService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countrystates")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class CountryStateController {
+
+    CountryStateService countryStateService;
+
+    @GetMapping
+    public Page<CountryState> countryStates(Pageable page) {
+        return countryStateService.listAll(page);
+    }
 }
